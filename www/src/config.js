ebce4e1e3f043f386bded8a9bf9057225a099df5
Anthony Morris <aadmorr@amazon.com>
2022-01-16 18:55:40 -0500

    tshoot

--- www/src/config.js
+++ www/src/config.js
@@ -3,7 +3,7 @@
 
 const config = {
   "aws_user_pools_web_client_id": "7pupa4snasvbeushd59g6lm20v",     // CognitoClientID
-  "api_base_url": "https://936apsovxf.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "api_base_url": "https://936apsovxf.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
   "cognito_hosted_domain": "webapp-todo-webapp-todo.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
   "redirect_url": "https://master.d1ovqnzixst3ct.amplifyapp.com"                                       // AmplifyURL
 };
