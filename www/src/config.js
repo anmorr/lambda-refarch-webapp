a38666f801d468c29a92a51c8ca76d6f8a942890
Anthony Morris <aadmorr@amazon.com>
2022-01-16 18:11:26 -0500

    updated config

--- www/src/config.js
+++ www/src/config.js
@@ -5,7 +5,7 @@ const config = {
   "aws_user_pools_web_client_id": "7pupa4snasvbeushd59g6lm20v",     // CognitoClientID
   "api_base_url": "https://936apsovxf.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
   "cognito_hosted_domain": "webapp-todo-webapp-todo.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://localhost:8080"                                       // AmplifyURL
+  "redirect_url": "https://master.d1ovqnzixst3ct.amplifyapp.com"                                       // AmplifyURL
 };
 
 export default config;
